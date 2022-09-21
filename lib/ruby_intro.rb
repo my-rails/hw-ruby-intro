a63d00593f3fca96718a17e8a51003e9f70d32db
Darius Pirvulescu <organicdarius@gmail.com>
2022-09-21 23:01:15 +0100

    Complete part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,42 @@
 
 def sum arr
   # YOUR CODE HERE
+  return 0 if arr&.empty?
+
+  # arr.sum
+  # arr.reduce(:+)
+  
+  total = 0
+  arr.each do |e|
+    total += e
+  end
+  total
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  return 0 if arr&.empty?
+
+  return arr.first if arr.count == 1
+
+  # Check solution without sort
+  sorted = arr.sort
+  a = sorted[-1]
+  b = sorted[-2]
+  a + b
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  return false if arr&.empty? || arr&.count == 1
+
+  for i in arr do
+    for j in arr do
+      i, j = j, i if i > j
+      return true if i + j == n
+    end
+  end
+  return false
 end
 
 # Part 2

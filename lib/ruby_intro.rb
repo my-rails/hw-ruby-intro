67ff9c78b21fe29afdd93b80990ed5761724e73e
Darius Pirvulescu <organicdarius@gmail.com>
2022-09-22 20:25:01 +0100

    Finish 2.1 and 2.2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -46,10 +46,18 @@ end
 
 def hello(name)
   # YOUR CODE HERE
+
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  return false if s == ''
+
+  # s[0].match?(/[^aeiou|\W]/i)
+
+  # Also written
+  /[^aeiou|\W]/i =~ s[0]
 end
 
 def binary_multiple_of_4? s

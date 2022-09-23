4fb4d5c184954223188a4d05d8b4f180fc5d0e13
Darius Pirvulescu <organicdarius@gmail.com>
2022-09-23 19:19:00 +0100

    Complete ruby_intro

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -79,4 +79,17 @@ end
 
 class BookInStock
 # YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError, 'No ISBN' if isbn == ''
+    raise ArgumentError, 'Invalid price' if price <= 0
+    
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    "$#{"%0.2f" % price}"
+  end
 end
